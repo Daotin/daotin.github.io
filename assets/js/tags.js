38306cba51a98d1d26b8ed1b92351804788f1043
Daotin <daotin@qq.com>
2021-06-30 22:52:24 +0800

    'add'

--- assets/js/tags.js
+++ assets/js/tags.js
@@ -1,6 +1,17 @@
 ---
 # Liquid pretreatment required
 ---
+    
+function getQuery(key) {
+    var query = window.location.search.substring(1);
+    var map = query.split("&");
+    for (var i = 0; i < map.length; i++) {
+        var pair = map[i].split("=");
+        if (pair[0] == key) {
+        return pair[1];
+        }
+    }
+}
 
 var keyword = getQuery('keyword');
 
